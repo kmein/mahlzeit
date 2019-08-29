aea8469e5095924ee7863f4f34f287c329a938b0
Kierán Meinhardt <kieran.meinhardt@gmail.com>
2019-08-29 08:09:06 +0200

    feat(pretty printer): add initial pretty printer

--- default.nix
+++ default.nix
@@ -1,11 +1,11 @@
-{ mkDerivation, base, megaparsec, stdenv, text }:
+{ mkDerivation, base, megaparsec, pretty, stdenv, text }:
 mkDerivation {
   pname = "mahlzeit";
   version = "0.1.0";
   src = ./.;
   isLibrary = false;
   isExecutable = true;
-  executableHaskellDepends = [ base megaparsec text ];
+  executableHaskellDepends = [ base megaparsec pretty text ];
   homepage = "https://github.com/kmein/mahlzeit";
   description = "Recipe parser";
   license = stdenv.lib.licenses.mit;
--- src/Main.hs
+++ src/Main.hs
@@ -1,17 +1,18 @@
 {-# LANGUAGE FlexibleContexts #-}
 module Main where
 
-import Parser 
+import Parser (parseMeal)
 import Types
+import PrettyPrinter (renderMeal)
 
 import Text.Megaparsec
 import Data.Text (Text)
 import qualified Data.Text as Text
+import qualified Data.Text.IO as Text
 
 main :: IO ()
 main = do
-  input <- Text.pack <$> getContents
-  parseTest parseMeal input
-  -- case parse parseMeal "(stdin)" input of
-  --   Right result -> print result
-  --   Left e -> print e
+  input <- Text.getContents
+  case parse parseMeal "(stdin)" input of
+    Right meal -> Text.putStrLn $ renderMeal meal
+    Left e -> print e
